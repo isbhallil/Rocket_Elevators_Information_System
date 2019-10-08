53fe68f1cb4d86cd04e4c8b671e79ea2d9d65d97
mo bhallil <is.bhallil@gmail.com>
2019-10-08 16:41:36 -0400

    not working - pending resolution

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
 //= link_tree ../images
+//= link_tree ../images/slider-image
+//= link_tree ../images/portfolio-images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link_tree ../images/ png
