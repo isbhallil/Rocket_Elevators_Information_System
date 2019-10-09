f77e76badcacdca0ead711af5f069c4bcea9b453
gbibeau1 <gabriel_bibeau@hotmail.com>
2019-10-09 16:29:56 -0400

    bob

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,6 @@
 //= link_tree ../images
-<<<<<<< HEAD
-=======
 //= link_tree ../images/slider-image
 //= link_tree ../images/portfolio-images
 //= link_directory ../javascripts .js
->>>>>>> 63f5b840605917f21e33ad9be70900b3258eac0e
 //= link_directory ../stylesheets .css
 //= link_tree ../images/ 
