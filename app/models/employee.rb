366d504aaea4e641550695f7430f5c784d15e659
Patster052 <cavanagh.patrick@gmail.com>
2019-10-11 11:27:58 -0400

    fridaymidi

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,5 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable
+  devise :database_authenticatable, :recoverable, :rememberable
 end
