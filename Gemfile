4c3882dc459ab61e86608aaab7e7f4c4f796dd98
cedrick <cedgag1@gmail.com>
2019-10-15 12:52:28 -0400

    uploader and table created

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
+gem 'carrierwave', '~> 2.0', '>= 2.0.2'
 gem 'jquery-rails'
 gem "font-awesome-rails"
 gem 'owlcarousel-rails'
---
+++ db/migrate/20191015161923_create_leads.rb
@@ -0,0 +1,17 @@
+class CreateLeads < ActiveRecord::Migration[6.0]
+  def change
+    create_table :leads do |t|
+      t.string :name
+      t.string :full_name
+      t.string :enterprise_name
+      t.string :email
+      t.string :phone_number
+      t.string :immovable_project_name
+      t.string :project_description
+      t.string :departement_in_charge_of_elevators
+      t.string :message
+      t.string :image
+      t.timestamps
+    end
+  end
+end
