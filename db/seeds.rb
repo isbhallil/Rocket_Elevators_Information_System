e4e5ba3cd8d2b8076d83efc99d186129a955fb29
gbibeau1 <gabriel_bibeau@hotmail.com>
2019-10-18 11:37:54 -0400

    moh merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -250,13 +250,14 @@ csv_text = File.read(Rails.root.join('lib', 'seed', 'buildingdetail.csv'))
     csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 
     csv.each do |row| 
+        t = BuildingDetail.new 
         t.building_id = row['building_id']
         t.info_key = row['info_key']
         t.value = row['value']
         t.created_at = row['created_at']
         t.updated_at = row['updated_at']
         t.save!
-        t = BuildingDetail.new  
+        
         
 end 
 
